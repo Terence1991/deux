06fa2907511741043365965ca25aa27eb67e1d6f
terence1991 <cplterencehawes@gmail.com>
2019-11-15 11:39:59 -0500

    flex dirextion row

--- src/App.js
+++ src/App.js
@@ -18,3 +18,13 @@ const CardsContainter = styled.div`
   border: dotted;
   height: 100vh;
 `
+ const cardRow1 = styled.div`
+
+ `
+ const cardRow2 = styled.div`
+ 
+ `
+
+ const cardRow3 = styled.div`
+ 
+ `
