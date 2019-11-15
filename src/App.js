75b06c6ce4ee95864ee52821859cdb94f428f9c3
terence1991 <cplterencehawes@gmail.com>
2019-11-15 11:18:13 -0500

    box set up for flex box

--- src/App.js
+++ src/App.js
@@ -5,29 +5,16 @@ import styled from 'styled-components';
 
 const App = () => {
   return (
-    <AppContainer>
       <CardsContainter>
         App
       </CardsContainter>
-    </AppContainer>
   )
 }
 
 export default App;
 
-const AppContainer = styled.div`
-  font-family: 'Roboto', sans-serif;
-`
 const CardsContainter = styled.div`
   display: flex;
-  flex-direction: row;
-  max-width: 800px;
-  border-style: solid;
-  border-color:  red;
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  overflow: auto;
+  border: dotted;
+  height: 100vh;
 `
